cabb94d5b2b286500fa7a7a38c593cba7ea79204
Moumen A. <Alkatawnehm@Gmail.com>
2025-09-07 19:39:28 -0400

    updated jinja files! :)

--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Welcome"
 
 
+@app.route("/head")
+def head():
+    render_template("index.html")
+    return render_template("index.html", number1=123, number2=777)        
 
+@app.route("/sum")
+def sum():
 
-
+    val1 = 55
+    val2 = 125
+    mysum = val1 + val2
+    return render_template("body.html", value1 = val1, value2 = val2, sum = mysum)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run(host="0.0.0.0")
